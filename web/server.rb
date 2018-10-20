21420a756cf5dc0e523fbfefc28f5dcdd082fc6b
T.Tomita <ttp8101@gmail.com>
2018-10-20 18:45:36 +0900

    Use SecureRandom.uuid as a temporary file name

--- web/server.rb
+++ web/server.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'haml'
 
 require 'fileutils'
+require 'securerandom'
 
 require_relative './convertor.rb'
 
@@ -15,14 +16,27 @@ post "/upload" do
     @img_file = params[:file]
 
     p "#{@img_file}"
+    uuid = SecureRandom.uuid
 
     input_file_path = @img_file[:tempfile].path
-    res_file_path   = "./tmp/res.md"
-    Convertor::convert(input_file_path, res_file_path)
-    content_type Convertor.file_type(res_file_path)
-    attachment res_file_path
-    File.read(res_file_path)
+    @res_file_path  = File.join("tmp", "#{uuid}.md")
+
+    Convertor::convert(input_file_path, @res_file_path)
+    content_type Convertor.file_type(@res_file_path)
+
+    input_file_name = @img_file[:filename]
+    attachment_name = File.basename(input_file_name, File.extname(input_file_name))
+    attachment "#{attachment_name}.md"
+    File.read(@res_file_path)
+
+    STDERR.puts "#{attachment_name}.md"
   else
     @msg = "Error"
+  end
+end
+
+after "/upload" do
+  if Dir[@res_file_path]
+    FileUtils.rm_rf(@res_file_path)
   end
 end
