0e6036e07b28b46a03824f2a0861be873eeb879c
T.Tomita <ttp8101@gmail.com>
2018-10-20 14:00:30 +0900

    Added file upload form to index

--- web/server.rb
+++ web/server.rb
@@ -1,6 +1,24 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'haml'
 
 get "/" do
-  "Hello world!"
+  haml :index
+end
+
+post "/upload" do
+  if params[:file]
+    @img_file = params[:file]
+
+    p "#{@img_file}"
+
+    save_path = "./tmp.png"
+    File.open(save_path, 'wb') do |f|
+      f.write @img_file[:tempfile].read
+    end
+
+    @msg = "The image has succesfully uploaded"
+  else
+    @msg = "Error"
+  end
 end
